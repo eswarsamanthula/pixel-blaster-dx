01b812da9951ec5ff30c4f137b56381e84a9c589
eswarsamanthula <eswarsamanthulas@gmail.com>
2026-06-07 12:19:44 +0530

    fix: mobile pause btn, CSS variables, achievement gallery, SW cache, manifest, myRole

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='pixel-blaster-dx-v1';
-const URLS=['./','./index.html','./manifest.json'];
+const CACHE='pixel-blaster-dx-'+(self.registration?.active?.installedTimestamp||Date.now());
+const URLS=['./','./index.html','./manifest.json','https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js'];
 
 self.addEventListener('install',e=>{
   e.waitUntil(
